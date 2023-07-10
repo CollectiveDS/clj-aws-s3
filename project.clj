6ddadc76b78715b88881050db589be7f07c1d32c
Ben Slawski <bslawski01@gmail.com>
2023-07-10 10:57:52 -0500

    upgrade clojure.core to 1.10.1, aws.java.sdk to 1.12.441

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/clj-aws-s3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.amazonaws/aws-java-sdk "1.11.130"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.amazonaws/aws-java-sdk "1.12.441"]
                  [clj-time "0.13.0"]]
   :plugins [[codox "0.8.10"]])
